8b231c1c5e432a7c8974e48d1c8ba1635fd948b2
Labanane1 <143994406+Labanane1@users.noreply.github.com>
2024-05-21 12:09:22 +0300

    création d'une classe kotlin des données

--- app/src/main/java/com/example/affirmation/MainActivity.kt
+++ app/src/main/java/com/example/affirmation/MainActivity.kt
@@ -8,39 +8,36 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.example.affirmation.model.Affirmation
 import androidx.compose.ui.Modifier
+import androidx.compose.material3.Card
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.affirmation.ui.theme.AffirmationTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            AffirmationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+@Composable
+fun AffirmationsApp() {
+}
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    Card(modifier = modifier){
+        Column {
+            Image(
+                painter = painterResource(affirmation.imageResourceId),
+                contentDescription = stringResource(affirmation.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop
+            )
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AffirmationTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/affirmation/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmation/model/Affirmation.kt
@@ -1,3 +1,13 @@
 package com.example.affirmation.model
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 
-data class Affirmation()
+data class affirmation(
+    val stringResourceId: Int,
+    val imageResourceId: Int
+)
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
