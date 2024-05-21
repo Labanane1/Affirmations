160b43d4aa7e8350a4762827149586bb550b8415
Labanane1 <143994406+Labanane1@users.noreply.github.com>
2024-05-21 12:48:26 +0300

    création de la classe main pour l'affichage

--- app/src/main/java/com/example/affirmation/MainActivity.kt
+++ app/src/main/java/com/example/affirmation/MainActivity.kt
@@ -1,28 +1,56 @@
 package com.example.affirmation
 
 
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import com.example.affirmation.model.Affirmation
-import androidx.compose.ui.Modifier
-import androidx.compose.material3.Card
-import androidx.compose.foundation.layout.Column
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.material3.Text
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmation.data.DataSources
+import com.example.affirmation.model.Affirmation
+import com.example.affirmation.ui.theme.AffirmationTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AffirmationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AffirmationApp()
+                }
+            }
+        }
+    }
+}
 
 @Composable
-fun AffirmationsApp() {
+fun AffirmationApp() {
+    AffirmationList(
+        affirmationList = DataSources().loadAffirmations(),
+    )
 }
 
 @Composable
@@ -48,7 +76,12 @@ fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
 @Composable
 fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
     LazyColumn(modifier = modifier) {
-
+        items(affirmationList) {affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
     }
 }
 @Preview
