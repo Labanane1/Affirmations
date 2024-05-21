4590686d03853f30fb33848998cc12fa0977777a
Labanane1 <143994406+Labanane1@users.noreply.github.com>
2024-05-21 12:18:18 +0300

    prévisualisation du code

--- app/src/main/java/com/example/affirmation/MainActivity.kt
+++ app/src/main/java/com/example/affirmation/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.example.affirmation
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import com.example.affirmation.model.Affirmation
@@ -19,8 +15,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.affirmation.ui.theme.AffirmationTheme
 
 @Composable
 fun AffirmationsApp() {
@@ -38,6 +36,16 @@ fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
                     .height(194.dp),
                 contentScale = ContentScale.Crop
             )
+            Text(
+                text = LocalContext.current.getString(affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
         }
     }
+}
+@Preview
+@Composable
+private fun AffirmationCardPreview() {
+
 }
